361e29bda0c7d92706cb671ca1c644cae7af8309
Shreyas Ganesh <vansi.ganeshs@gmail.com>
2024-12-10 23:25:31 -0500

    Create test file; '

--- fileopening.c
+++ fileopening.c
@@ -12,6 +12,7 @@ typedef struct{
     unsigned int filelength;
 } database_header;
 
+
 int main() {
     char* db_file_path = "my-db.db";
     int fd = open(db_file_path, O_RDONLY | O_CREAT, 0644);
@@ -21,6 +22,10 @@ int main() {
     }
 
     database_header* buf;
+    struct stat* dbstat;
+    if (fstat(fd, dbstat) < 0){
+        perror("fstat");
+        return -1;}
     int nb = read(fd, buf, sizeof(database_header));
     if (nb!= sizeof(database_header)){
         perror("read");
@@ -28,6 +33,10 @@ int main() {
         return -1;
     }
 
+    if( dbstat->st_size != buf->filelength){
+        printf("illegal file size: %d \n", buf->filelength);
+        return -1;
+    }
     printf("version is %u\n", buf->version);
     printf("number of employees is %u\n", buf->employees);
     printf("filelength is %u\n", buf->filelength);
